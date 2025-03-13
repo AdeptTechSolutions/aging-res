1683f27cc876087c4cce2e7c8112c7c3e76859d7
muhd-umer <thisismumer@gmail.com>
2025-03-13 19:01:17 +0500

    Fix pdf display

--- document_processor.py
+++ document_processor.py
@@ -102,7 +102,7 @@ class DocumentProcessor:
             ),
             ("text_converter", TextFileToDocument()),
             ("markdown_converter", MarkdownToDocument()),
-            ("pdf_converter", PyMuPDFToDocument()),
+            ("pdf_converter", PyPDFToDocument()),
             ("joiner", DocumentJoiner()),
             ("enricher", MetadataEnricher(metadata_dict=self.metadata_dict)),
             ("cleaner", DocumentCleaner()),
